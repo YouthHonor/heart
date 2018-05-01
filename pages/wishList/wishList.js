a3156df32dca3561410a973e3f58947037591a93
gh <18662225601@163.com>
2018-05-01 15:06:10 +0800

    gh test

--- pages/wishList/wishList.js
+++ pages/wishList/wishList.js
@@ -1,3 +1,8 @@
 Page({
-    
+    bindPickerChange: function (e) {
+        console.log('picker发送选择改变，携带值为', e.detail.value)
+        this.setData({
+            index: e.detail.value
+        })
+    }
 })
