a68451b9264d9bc0a0aff3852badd7f9909a2f43
zhanp <751196943@qq.com>
2018-05-01 19:34:56 +0800

    zhanp submmit verify page

--- pages/verify/verify.js
+++ pages/verify/verify.js
@@ -1,10 +1,16 @@
 Page({
   data:{
-    infoArray:[]
-  },
+    /*加个id属性*/
+    infoArray: [{ name: "小静", phoneNumber: "17826267128", address: "长春", date: "2018-5-1",      passFlag: "null"}
+                ],
+    ifPass:[{pass:"true",value:"通过审核",checked:true},
+            {pass:"false",value:"拒绝申报"}
+            ]
+    },
   onLoad:function(){
     var that = this;
     /*获取那边的相应的未审核的心愿或者服务信息,填到数组里*/
+    /*
     wx.request({
       url: '',
       method:'GET',
@@ -13,7 +19,7 @@ Page({
       },
       success:function(res){
         var data = res.data;
-        /*假设返回的是一个JSON对象数组*/
+        
         that.setData({
           infoArray:data
         })
@@ -25,6 +31,37 @@ Page({
         console.log("get success")
       }
     })
+    */
   },
+  radioChange:function(e){
+    var id = e.currenTarget.dataset.xyId;
+    var str = "infoArray["+id+"]"+".passFlag"
+    this.setData({
+      /*假设有个字段是审核状态的标志*/
+      /*这里动态修改了infoArray里的passFlag,然后再把新的请求信息传过去*/
+      [str] : e.currentTarget.dataset.passStatus
+    })
+  },
+  submit:function(){
+    /*后端是接收JSON数据的,算了吧，让他更新一下数据库把233*/
+    var that = this;
+    wx.request({
+      url: '',
+      method:"GET",
+      data:{
+        infoArray:that.data.infoArray
+      },
+      header: {
+        "Content-Type": "json"
+      },
+      success:function(res){
+        wx.navigateTo({
+          /*跳回到入口页面*/
+          url: '',
+        })
+      }
+
+    })
+  }
 
 })
