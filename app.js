bc4069ec177e8642893e3e3a31736dcfedeaecc7
zhanp <751196943@qq.com>
2018-05-07 22:50:41 +0800

    zhanp submit app.js after getting open-id

--- app.js
+++ app.js
@@ -1,3 +1,35 @@
-{
-    
-}
+//app.js
+App({
+  onLaunch: function () {
+    // 展示本地存储能力
+
+    wx.login({
+      success: function (res) {
+        console.log(res.code);
+        if (res.code) {
+          //发起网络请求
+          var that = this;
+          wx.request({
+            url: 'http://localhost:8080/login',
+            data: {
+              code: res.code
+            },
+            success: function (res) {
+              getApp().globalData.open_id = res.data;
+              console.log(res.data);
+              console.log(getApp().globalData.open_id)
+            }
+          })
+        } else {
+          console.log('登录失败！' + res.errMsg)
+        }
+      }
+    })
+
+  },
+
+
+  globalData: {
+    open_id: null
+  }
+})
