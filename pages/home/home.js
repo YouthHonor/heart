70d56dfd3faf1bc979d5f73b635b922fa165f906
sunhao <1138298595@qq.com>
2018-05-14 07:22:22 +0800

    5.14

--- pages/home/home.js
+++ pages/home/home.js
@@ -3,11 +3,7 @@ var app = getApp();
 Page({
 
   onLoad: function (options) {
-    if(app.appData.userinfo == null){
-      wx.redirectTo({
-        url: '/pages/about/about',
-      })
-    }
+
   },
   f1: function(){
     wx.navigateTo({
